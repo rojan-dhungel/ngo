316e544c59d55e0d842cae069a60f309f02c42b7
Rojan Dhungel <rojandhungel11@gmail.com>
2023-09-24 13:11:41 +0545

    added back to the top button

--- src/All.jsx
+++ src/All.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './Head-Components/Header'
 import Home from './Home-Components/Home';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import BackToTopButton from './BackToTopButton';
 function All() {
   return (
     <>
       <Header/>
       <Home/>
+      <BackToTopButton/>
       
     </>
   )
